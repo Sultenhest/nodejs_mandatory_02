8dded7521edbd89bdb5dce1e4c49613abede62f9
Simon Konstantyner <simon@konstantyner.dk>
2018-01-04 13:23:55 +0100

    Using Router again..

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -1,5 +1,6 @@
 const express        = require( 'express' ),
-      app            = express(),
+      //app            = express(),
+      app      = express.Router(),
       cors           = require( 'cors' ),
       bodyParser     = require( 'body-parser' ),
       methodOverride = require( 'method-override' ),
